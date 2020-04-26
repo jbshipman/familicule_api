994479bf1b46cfeef1a5ceea91ab95718536dcd5
jbshipman <jbshipman@icloud.com>
2020-04-26 18:07:53 -0500

    update securty in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,16 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    origins "http://localhost:3000"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
   allow do
-    origins 'https://familicule-api.herokuapp.com' 
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    origins "https://familicule-api.herokuapp.com"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+
+  allow do
+    origins "https://familicule-app.herokuapp.com"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end

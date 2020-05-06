cbfa74be13316ea3511fed6e08ce75d7a6e21c79
jbshipman <jbshipman@icloud.com>
2020-05-06 14:05:22 -0500

    delete event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -67,35 +67,8 @@ class EventsController < ApplicationController
     end
   end
 
-  # def show
-  #   profile = Profile.find_by(id: params[:id])
-  #   render json: profile
-  # end
-
-  # def update
-  #   # grab current_user id to build a profile onto
-  #   # try is built in
-  #   # {profile: {firstname, lastname, ....}}
-  #   profile = Profile.find_by(id: params[:id])
-  #   profile.update(
-  #     firstname: params["profile"]["firstname"],
-  #     lastname: params["profile"]["lastname"],
-  #     bio: params["profile"]["bio"],
-  #     birthday: params["profile"]["birthday"],
-  #     # user_id: @current_user.id,
-  #   )
-
-  #   # conditional to render json object of a status notification, a boolean for logged in, and the user model data
-  #   if profile
-  #     session[:user_id] = @current_user.id
-  #     render json: {
-  #       status: :updated,
-  #       logged_in: true,
-  #       user: @current_user,
-  #       profile: @current_profile,
-  #     }
-  #   else
-  #     render json: { status: 401 }
-  #   end
-  # end
+  def destroy
+    event = Event.find_by(id: params[:id])
+    event.destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   resources :user_cules
   resources :cules, only: [:index, :show, :create]
-  resources :events, only: [:index, :show]
-  resources :events, only: [:create]
+  resources :events, only: [:index, :show, :create, :destroy]
+  # resources :events
   # get :index_all, to: "events"
 
   get :users_in_cule, to: "cules#users_in_cule"
